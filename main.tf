db199ca7e323d56936166574230aff54d966c3a6
ahmafar <57182043+ahmafar@users.noreply.github.com>
2019-11-21 12:14:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,3 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = "${var.location}"
 }
-module "web_app_container" {
-   source = "app.terraform.io/farnet/web-app-container/azurerm”
-   version = "2.2.1" 
-   name = "${var.prefix}"
-   port = "80"
-   https_only = "false"
-   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
-   container_type = "docker"
-   container_image = "scarolan/palacearcade"
-}
-output "container_app_url" {
-   value = "http://${module.web_app_container.hostname}"
-}
